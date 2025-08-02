f893bf39df5dbd61aaad4f29fa399fd6b0c487f9
cccfffiii <cccfffiii@qq.com>
2025-08-03 06:30:22 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+import config from './doc/doc.js'; // 不需要大括号 {}
+config.forEach(item => {
+    console.log(`a: ${item.a}, b: ${item.b}`);
+    
+    if (item.c) {
+        console.log('包含子项:');
+        item.c.forEach(subItem => {
+            console.log(`  a: ${subItem.a}, b: ${subItem.b}`);
+        });
+    }
+});
+
+
+
+
+
+
+
 const aside = document.getElementById('aside');
 const menu = document.getElementById('menu');
 menu.addEventListener('click', function() {
