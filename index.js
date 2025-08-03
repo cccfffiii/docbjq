f442e3fa8744ee6a13e581708799792995f67f98
cccfffiii <cccfffiii@qq.com>
2025-08-03 12:12:44 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-import config from './doc/doc.js'; // 不需要大括号 {}
-config.forEach(item => {
-    console.log(`a: ${item.a}, b: ${item.b}`);
-    
-    if (item.c) {
-        console.log('包含子项:');
-        item.c.forEach(subItem => {
-            console.log(`  a: ${subItem.a}, b: ${subItem.b}`);
-        });
+import config from './doc/doc.js';
+
+function inittheme() {
+    if (localStorage.getItem('theme')==='light') {
+        themeclick('light');
+    } else if (localStorage.getItem('theme')==='dark') {
+        themeclick('dark');
+    } else if (localStorage.getItem('theme')==='auto') {
+        themeclick('auto');
+    }
+    const savedtheme = localStorage.getItem('theme') || 'light';
+    settheme(savedtheme, false);
+    themeclick(savedtheme);
+}
+function settheme(theme,save=true) {
+    if (save) localStorage.setItem('theme',theme)
+    if (theme === 'dark' || (theme === 'auto' && window.matchMedia('(prefers-color-scheme:dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+}
+const themelight = document.getElementById('theme-light');
+const themedark= document.getElementById('theme-dark');
+const themeauto = document.getElementById('theme-auto');
+themelight.addEventListener('click', function() {
+    settheme('light');
+    themeclick('light');
+});
+themedark.addEventListener('click', function() {
+    settheme('dark');
+    themeclick('dark');
+});
+themeauto.addEventListener('click', function() {
+    settheme('auto');
+    themeclick('auto');
+});
+function themeclick(theme) {
+    console.log(theme);
+    if (theme === 'light') {
+        
+        themelight.classList.add('click');
+        themedark.classList.remove('click');
+        themeauto.classList.remove('click');
+    } else if (theme === 'dark') {
+        themelight.classList.remove('click');
+        themedark.classList.add('click');
+        themeauto.classList.remove('click');
+    } else if (theme === 'auto') {
+        themelight.classList.remove('click');
+        themedark.classList.remove('click');
+        themeauto.classList.add('click');
+    }
+}
+inittheme();
+window.matchMedia('(prefers-color-scheme:dark)').addEventListener('change', e => {
+    if (localStorage.getItem('theme') === 'auto') {
+        settheme('auto', false);
     }
 });
-
-
 
 
 
