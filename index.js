a3934259ff6722715931112038440edd393098a8
cccfffiii <cccfffiii@qq.com>
2025-08-04 11:04:59 +0800

    update

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-import config from './doc/doc.js';
-
-function inittheme() {
-    if (localStorage.getItem('theme')==='light') {
-        themeclick('light');
-    } else if (localStorage.getItem('theme')==='dark') {
-        themeclick('dark');
-    } else if (localStorage.getItem('theme')==='auto') {
-        themeclick('auto');
-    }
-    const savedtheme = localStorage.getItem('theme') || 'light';
-    settheme(savedtheme, false);
-    themeclick(savedtheme);
-}
-function settheme(theme,save=true) {
-    if (save) localStorage.setItem('theme',theme)
+//主题设置相关代码
+const theme = localStorage.getItem("theme") || 'auto';
+settheme(theme);
+window.matchMedia('(prefers-color-scheme:dark)').addEventListener('change', e => {
+    if (localStorage.getItem('theme') === 'auto') {
+       settheme("auto");
+   }
+});
+function settheme(theme) {
+    localStorage.setItem('theme', theme)
     if (theme === 'dark' || (theme === 'auto' && window.matchMedia('(prefers-color-scheme:dark)').matches)) {
         document.documentElement.classList.add('dark');
     } else {
@@ -21,56 +15,39 @@ function settheme(theme,save=true) {
     }
 }
 const themelight = document.getElementById('theme-light');
-const themedark= document.getElementById('theme-dark');
+const themedark = document.getElementById('theme-dark');
 const themeauto = document.getElementById('theme-auto');
 themelight.addEventListener('click', function() {
     settheme('light');
-    themeclick('light');
+    themelight.classList.add('click');
+    themedark.classList.remove('click');
+    themeauto.classList.remove('click');
 });
 themedark.addEventListener('click', function() {
     settheme('dark');
-    themeclick('dark');
+    themelight.classList.remove('click');
+    themedark.classList.add('click');
+    themeauto.classList.remove('click');
 });
 themeauto.addEventListener('click', function() {
     settheme('auto');
-    themeclick('auto');
-    const e=document;(e.fullscreenElement||e.webkitFullscreenElement||e.mozFullScreenElement||e.msFullscreenElement)?(e.exitFullscreen||e.webkitExitFullscreen||e.mozCancelFullScreen||e.msExitFullscreen).call(e):(e.documentElement.requestFullscreen||e.documentElement.webkitRequestFullscreen||e.documentElement.mozRequestFullScreen||e.documentElement.msRequestFullscreen).call(e.documentElement)
-});
-function themeclick(theme) {
-    console.log(theme);
-    if (theme === 'light') {
-        
-        themelight.classList.add('click');
-        themedark.classList.remove('click');
-        themeauto.classList.remove('click');
-    } else if (theme === 'dark') {
-        themelight.classList.remove('click');
-        themedark.classList.add('click');
-        themeauto.classList.remove('click');
-    } else if (theme === 'auto') {
-        themelight.classList.remove('click');
-        themedark.classList.remove('click');
-        themeauto.classList.add('click');
-    }
-}
-inittheme();
-window.matchMedia('(prefers-color-scheme:dark)').addEventListener('change', e => {
-    if (localStorage.getItem('theme') === 'auto') {
-        settheme('auto', false);
-    }
-});
+    themelight.classList.remove('click');
+    themedark.classList.remove('click');
+    themeauto.classList.add('click');
+})
 
 
 
 
 
+//显示隐藏侧边栏代码
 const aside = document.getElementById('aside');
-const menu = document.getElementById('menu');
-menu.addEventListener('click', function() {
+const menuicon = document.getElementById('menuicon');
+menuicon.addEventListener('click', function() {
     aside.classList.toggle('active');
     if (aside.classList.contains('active')) {
-        menu.src = "img/a2.svg"; 
+        menuicon.innerHTML = `<path d="M128 160h768v64H128v-64zM128 373.333333h512v64H128v-64zM128 586.666667h512v64H128v-64zM128 800h768v64H128v-64zM896 384l-170.666667 128 170.666667 128V384z"></path>`;
     } else {
-        menu.src = "img/a1.svg"; 
+        menuicon.innerHTML = `<path d="M128 160h768v64H128v-64zM128 373.333333h512v64H128v-64zM128 586.666667h512v64H128v-64zM128 800h768v64H128v-64zM725.333333 384l170.666667 128-170.666667 128V384z"></path>`; 
     }
 });
